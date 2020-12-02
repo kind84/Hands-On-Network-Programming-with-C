7f3d03d0ff7667df8fb40c055183e2bc975c84b8
Paolo Grisoli <paolo_g84@icloud.com>
2020-12-02 16:26:09 +0100

    idiomatic defer and use recv not recvfrom

--- chap02/time_server_dual_lowlvl.zig
+++ chap02/time_server_dual_lowlvl.zig
@@ -14,7 +14,10 @@ pub fn main() !void {
 
     print("Creating socket...\n", .{});
     const sock_listen = try os.socket(os.AF_INET6, os.SOCK_STREAM, os.IPPROTO_TCP);
-    errdefer os.close(sock_listen);
+    defer {
+        print("Closing listening socket...\n", .{});
+        os.close(sock_listen);
+    }
 
     // TODO: for now this fails. Sill not available in zig?
     try os.setsockopt(sock_listen, os.IPPROTO_IPV6, IPV6_V6ONLY, &mem.toBytes(@as(c_int, 0)));
@@ -30,13 +33,17 @@ pub fn main() !void {
     var accepted_addr: net.Address = undefined;
     var addr_len: os.socklen_t = @sizeOf(net.Address);
     var sock_client = try os.accept(sock_listen, &accepted_addr.any, &addr_len, os.SOCK_CLOEXEC);
+    defer {
+        print("Closing connection...\n", .{});
+        os.close(sock_client);
+    }
 
     print("Client is connected...\n", .{});
     print("{}\n", .{accepted_addr});
 
     print("Reading request...\n", .{});
     var request: [1024]u8 = undefined;
-    const bytes_recv = os.recvfrom(sock_client, request[0..], 0, &accepted_addr.any, &addr_len);
+    const bytes_recv = os.recv(sock_client, request[0..], 0);
     print("Received {} bytes.\n", .{bytes_recv});
 
     print("Sending response...\n", .{});
@@ -50,12 +57,6 @@ pub fn main() !void {
     defer gpa.allocator.free(now_str);
     bytes_sent = os.send(sock_client, now_str, 0);
     print("Sent {} of {} bytes.\n", .{ bytes_sent, now_str.len });
-
-    print("Closing connection...\n", .{});
-    os.close(sock_client);
-
-    print("Closing listening socket...\n", .{});
-    os.close(sock_listen);
 
     print("Finished...\n", .{});
 }
